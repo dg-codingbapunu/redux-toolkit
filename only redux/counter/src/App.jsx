7165988aa156703ec473dce13f9e64c0a047ea17
patitapaban <patitapabanpanda046@gmail.com>
2024-05-20 14:47:25 +0530

    MAKE BUTTON S USING Bootstrap

--- only redux/counter/src/App.jsx
+++ only redux/counter/src/App.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
 import Container from "./Container";
+import Controls from "./components/Controls";
 const App = () => {
   return (
     <>
@@ -11,20 +12,7 @@ const App = () => {
           <Header></Header>
           <div className="col-lg-6 mx-auto">
             <DisplayCounter></DisplayCounter>
-            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 gap-3"
-              >
-                Primary button
-              </button>
-              <button
-                type="button"
-                className="btn btn-outline-secondary btn-lg px-4"
-              >
-                Secondary
-              </button>
-            </div>
+            <Controls></Controls>
           </div>
         </Container>
       </center>
