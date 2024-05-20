70c5bf199b78dc3a20f52651c27f067b3c663b3e
patitapaban <patitapabanpanda046@gmail.com>
2024-05-20 13:03:23 +0530

    action types

--- only redux/redux-kit.js
+++ only redux/redux-kit.js
@@ -8,6 +8,8 @@ const reducer = (store = INITIAL_VALUE, action) => {
     newStore = { counter: store.counter + 1 };
   } else if (action.type === "DECREMENT") {
     newStore = { counter: store.counter - 1 };
+  } else if (action.type === "ADDITION") {
+    newStore = { counter: store.counter + action.payload.number };
   }
   return newStore;
 };
@@ -20,4 +22,4 @@ store.subscribe(subscriber);
 store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "DECREMENT" });
 store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "ADDITION", payload: { number: 7 } });
