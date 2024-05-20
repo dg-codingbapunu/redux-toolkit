b7e0eb290dd232393e3b930739da1723840867f1
patitapaban <patitapabanpanda046@gmail.com>
2024-05-20 14:37:53 +0530

    make all components

--- only redux/counter/src/App.jsx
+++ only redux/counter/src/App.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components.jsx/Header";
-import DisplayCounter from "./components.jsx/DisplayCounter";
+import Header from "./components/Header";
+import DisplayCounter from "./components/DisplayCounter";
+import Container from "./Container";
 const App = () => {
   return (
     <>
-      <div className="px-4 py-5 my-5 text-center">
-        <Header></Header>
-        <div className="col-lg-6 mx-auto">
-          <DisplayCounter></DisplayCounter>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
-              Primary button
-            </button>
-            <button
-              type="button"
-              className="btn btn-outline-secondary btn-lg px-4"
-            >
-              Secondary
-            </button>
+      <center className="px-4 py-5 my-5 text-center">
+        <Container>
+          <Header></Header>
+          <div className="col-lg-6 mx-auto">
+            <DisplayCounter></DisplayCounter>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <button
+                type="button"
+                className="btn btn-primary btn-lg px-4 gap-3"
+              >
+                Primary button
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-secondary btn-lg px-4"
+              >
+                Secondary
+              </button>
+            </div>
           </div>
-        </div>
-      </div>
+        </Container>
+      </center>
     </>
   );
 };
